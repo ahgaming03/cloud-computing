ad7e2317ca8b51afb97c5b06a7de31bd7925c7dd
ahgaming03 <85728869+ahgaming03@users.noreply.github.com>
2023-09-19 00:14:13 +0700

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://root:Abcd1234@mvc-example.elbom2r.mongodb.net/?retryWrites=true&w=majority";
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+    "mongodb+srv://root:Abcd1234@mvc-example.elbom2r.mongodb.net/?retryWrites=true&w=majority";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -8,7 +8,7 @@ const client = new MongoClient(uri, {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function run() {
@@ -17,7 +17,9 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        console.log(
+            "Pinged your deployment. You successfully connected to MongoDB!"
+        );
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
